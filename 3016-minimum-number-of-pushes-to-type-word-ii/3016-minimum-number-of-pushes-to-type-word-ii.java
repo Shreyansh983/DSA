28129ea093d7aa5a2035e96cc4bee621e7ef9cf2
Shreyansh983 <88841210+Shreyansh983@users.noreply.github.com>
2026-08-01 05:25:40 +0530

    Time: 75 ms (5.75%), Space: 47.9 MB (72.09%) - LeetHub

---
+++ 3016-minimum-number-of-pushes-to-type-word-ii/3016-minimum-number-of-pushes-to-type-word-ii.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int minimumPushes(String word) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (int i = 0; i < word.length(); i++) {
+            map.put(word.charAt(i), map.getOrDefault(word.charAt(i), 0) + 1);
+        }
+
+        List<Integer> arr = new ArrayList<>();
+        for (Integer value : map.values()) {
+            arr.add(value);
+        }
+        Collections.sort(arr, Collections.reverseOrder());
+        System.out.println(arr);
+        int count = 0;
+        for (int i = 0; i < arr.size(); i++) {
+            if (i <= 7)
+                count += arr.get(i);
+            else if (i <= 15 && i > 7) {
+                count += arr.get(i)*2;
+            }else if(i <= 23 && i > 15){
+                count += arr.get(i)*3;
+            }else{
+                count += arr.get(i)*4;
+            }
+        }
+
+        return count;
+    }
+}
