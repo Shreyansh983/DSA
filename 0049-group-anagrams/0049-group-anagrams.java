f957be38f95e061267921420ecc17af7f8f5c12e
Shreyansh983 <88841210+Shreyansh983@users.noreply.github.com>
2026-08-04 00:45:10 +0530

    Time: 6 ms (99.24%), Space: 49.7 MB (58.64%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,9 +1,9 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-
+        // key -> sort sring hai jaise eat tea ate -> aet ban jayega aur key bhi aet rahega
         Map<String, List<String>> map = new HashMap<>();
         for (String s : strs) {
-            char[] chars = s.toCharArray();
+            char[] chars = s.toCharArray(); // for sorting
             Arrays.sort(chars);
             String sorted = new String(chars);
 
