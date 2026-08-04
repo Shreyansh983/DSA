cedaad349ad79cc6e55b1d9766056674e3fe1bb2
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-04 18:53:15 +0530

    Time: 4 ms (99.02%), Space: 47.9 MB (12.14%) - LeetHub

--- 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
+++ 0680-valid-palindrome-ii/0680-valid-palindrome-ii.java
@@ -1,20 +1,19 @@
 class Solution {
-    private boolean isPalindrome(int i, int j, StringBuilder sb){
+    private boolean isPalindrome(int i, int j, String s){
          while(i<j){
-            if(sb.charAt(i)!=sb.charAt(j)) return false;
+            if(s.charAt(i)!=s.charAt(j)) return false;
             i++;
             j--;
         }
         return true;
     }
     public boolean validPalindrome(String s) {
-        StringBuilder sb = new StringBuilder(s);
         int i = 0;
-        int j = sb.length()-1;
+        int j = s.length()-1;
         int count = 0;
         while(i<j){
-            if(sb.charAt(i)!=sb.charAt(j)){
-                return isPalindrome(i+1,j,sb) || isPalindrome(i,j-1,sb);
+            if(s.charAt(i)!=s.charAt(j)){
+                return isPalindrome(i+1,j,s) || isPalindrome(i,j-1,s);
             } 
             i++;
             j--;
