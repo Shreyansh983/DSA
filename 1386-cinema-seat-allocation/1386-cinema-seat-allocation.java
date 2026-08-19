84e94ad965378e06f27daf7d5ca93c5d33791625
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-19 22:12:14 +0530

    Time: 29 ms (41.32%), Space: 57.3 MB (9.74%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -11,37 +11,29 @@ class Solution {
         }
 
         int ans = (n - map.size()) * 2;
-
         for (Set<Integer> set : map.values()) {
-
             boolean left = true;
             boolean middle = true;
             boolean right = true;
 
-            // 2,3,4,5
             for (int seat = 2; seat <= 5; seat++) {
                 if (set.contains(seat)) {
                     left = false;
                     break;
                 }
             }
-
-            // 4,5,6,7
             for (int seat = 4; seat <= 7; seat++) {
                 if (set.contains(seat)) {
                     middle = false;
                     break;
                 }
             }
-
-            // 6,7,8,9
             for (int seat = 6; seat <= 9; seat++) {
                 if (set.contains(seat)) {
                     right = false;
                     break;
                 }
             }
-
             if (left && right) {
                 ans += 2;
             } else if (left || middle || right) {
