fb9b9ce4316c5e3d62eecbf39c1f502e4774d91f
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-19 22:13:36 +0530

    Time: 38 ms (7.37%), Space: 57.4 MB (6.32%) - LeetHub

--- 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
+++ 1386-cinema-seat-allocation/1386-cinema-seat-allocation.java
@@ -12,28 +12,36 @@ class Solution {
 
         int ans = (n - map.size()) * 2;
         for (Set<Integer> set : map.values()) {
-            boolean left = true;
-            boolean middle = true;
-            boolean right = true;
+            // boolean left = true;
+            // boolean middle = true;
+            // boolean right = true;
 
-            for (int seat = 2; seat <= 5; seat++) {
-                if (set.contains(seat)) {
-                    left = false;
-                    break;
-                }
-            }
-            for (int seat = 4; seat <= 7; seat++) {
-                if (set.contains(seat)) {
-                    middle = false;
-                    break;
-                }
-            }
-            for (int seat = 6; seat <= 9; seat++) {
-                if (set.contains(seat)) {
-                    right = false;
-                    break;
-                }
-            }
+            // for (int seat = 2; seat <= 5; seat++) {
+            //     if (set.contains(seat)) {
+            //         left = false;
+            //         break;
+            //     }
+            // }
+            // for (int seat = 4; seat <= 7; seat++) {
+            //     if (set.contains(seat)) {
+            //         middle = false;
+            //         break;
+            //     }
+            // }
+            // for (int seat = 6; seat <= 9; seat++) {
+            //     if (set.contains(seat)) {
+            //         right = false;
+            //         break;
+            //     }
+            // }
+            boolean left  = !set.contains(2) && !set.contains(3)
+                         && !set.contains(4) && !set.contains(5);
+
+            boolean middle = !set.contains(4) && !set.contains(5)
+                          && !set.contains(6) && !set.contains(7);
+
+            boolean right = !set.contains(6) && !set.contains(7)
+                         && !set.contains(8) && !set.contains(9);
             if (left && right) {
                 ans += 2;
             } else if (left || middle || right) {
