c2d74d2061f161507bb5ff1a73b4bb9d6de2ab3a
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-26 02:46:14 +0530

    Time: 10 ms (96.97%), Space: 47.3 MB (38.04%) - LeetHub

--- 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
+++ 1047-remove-all-adjacent-duplicates-in-string/1047-remove-all-adjacent-duplicates-in-string.java
@@ -1,14 +1,26 @@
 class Solution {
     public String removeDuplicates(String s) {
-        Deque<Character> stack = new ArrayDeque<>();
-        for(char ch : s.toCharArray()){
-            if(!stack.isEmpty() && stack.peek()==ch) stack.pop();
-            else stack.push(ch);
-        }
+        // Deque<Character> stack = new ArrayDeque<>();
+        // for(char ch : s.toCharArray()){
+        //     if(!stack.isEmpty() && stack.peek()==ch) stack.pop();
+        //     else stack.push(ch);
+        // }
+        // StringBuilder sb = new StringBuilder();
+        // while(!stack.isEmpty()){
+        //     char c = stack.removeLast();
+        //     sb.append(c);
+        // }
+        // return sb.toString();
+        //Using StringBuilder as a Stack
+
         StringBuilder sb = new StringBuilder();
-        while(!stack.isEmpty()){
-            char c = stack.removeLast();
-            sb.append(c);
+        for (char c : s.toCharArray()) {
+            int n = sb.length();
+            if (n > 0 && sb.charAt(n - 1) == c) {
+                sb.deleteCharAt(n - 1);
+            } else {
+                sb.append(c);
+            }
         }
         return sb.toString();
     }
