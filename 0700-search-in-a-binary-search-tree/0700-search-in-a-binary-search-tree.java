043486f9eed68a52947b4b6fe1b18a89b6346dea
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-26 02:58:44 +0530

    Time: 0 ms (100%), Space: 46.9 MB (54.57%) - LeetHub

--- 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
+++ 0700-search-in-a-binary-search-tree/0700-search-in-a-binary-search-tree.java
@@ -14,14 +14,16 @@
  * }
  */
 class Solution {
+    TreeNode ans = null;
     public TreeNode searchBST(TreeNode root, int val) {  
-        if (root == null) return null;
+        if (root == null) return ans;
         if (root.val == val) {
             return root;
         }
         if (val < root.val) {
-            return searchBST(root.left, val);
+            ans = searchBST(root.left, val);
         }
-        return searchBST(root.right, val);
+        ans = searchBST(root.right, val);
+        return ans;
     }
 }
