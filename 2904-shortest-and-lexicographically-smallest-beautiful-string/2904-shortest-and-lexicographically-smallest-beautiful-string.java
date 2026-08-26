ac9d4cdfdf066836c847c20edcf00088e6324526
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-08-26 16:06:14 +0530

    Time: 1 ms (100%), Space: 44 MB (58.39%) - LeetHub

--- 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
+++ 2904-shortest-and-lexicographically-smallest-beautiful-string/2904-shortest-and-lexicographically-smallest-beautiful-string.java
@@ -3,29 +3,27 @@ class Solution {
         char[] arr = s.toCharArray();
         int count = 0,i = 0;
         int min = Integer.MAX_VALUE;
-        StringBuilder sb = new StringBuilder();
-        StringBuilder ans = new StringBuilder();
+        String ans = "";
         for (int j = 0; j < arr.length; j++) {
             if (arr[j] == '1')
                 count++;
-            sb.append(arr[j]);
             while (i < j && (count > k || arr[i] == '0')) {
                 if (arr[i] == '1')
                     count--;
-                    sb.deleteCharAt(0);
                 i++;
             }
             if (count == k) {
+                String sb = s.substring(i,j+1);
                 if (sb.length() < min) {
                     min = sb.length();
-                    ans = new StringBuilder(sb);
+                    ans = sb;
                 } else if (sb.length() == min) {
-                    if (sb.toString().compareTo(ans.toString()) < 0) { //lexographically smallest
-                        ans = new StringBuilder(sb);
+                    if (sb.compareTo(ans) < 0) { //lexographically smallest
+                        ans = sb;
                     }
                 }
             }
         }
-        return ans.toString();
+        return ans;
     }
 }
