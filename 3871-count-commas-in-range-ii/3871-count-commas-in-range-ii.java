f5108ae2c2ec2e36710cbfd92d5dd9ced1daa6dc
Shreyansh Kumar <88841210+Shreyansh983@users.noreply.github.com>
2026-09-09 16:10:19 +0530

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 3871-count-commas-in-range-ii/3871-count-commas-in-range-ii.java
@@ -0,0 +1,34 @@
+class Solution {
+    public long countCommas(long n) {
+        long count = 0;
+        if(n<1000) return count;
+        if(n>=1000 && n<=999999) count = n-999;
+        if(n>999999){
+            count = n - 999;
+            long num = n - 999999;
+            count += num;
+        }
+        if(n>999999999){
+            count = n - 999;
+            long num = n - 999999;
+            long num2 = n - 999999999;
+            count += num + num2;
+        }
+        if(n > 999999999999L){
+            count = n - 999;
+            long num = n - 999999;
+            long num2 = n - 999999999;
+            long num3 = n - 999999999999L;
+            count += num + num2 + num3;
+        }
+        if(n > 999999999999999L){
+            count = n - 999;
+            long num = n - 999999;
+            long num2 = n - 999999999;
+            long num3 = n - 999999999999L;
+            long num4 = n - 999999999999999L;
+            count += num + num2 + num3;
+        }
+        return count;
+    }
+}
